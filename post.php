dd413ce68b27e34ce308edeeed108cb1e2b4dab0
Clay Flannery <johndoe@example.comcflannery94@gmail.com>
2021-09-12 20:33:22 -0400

    init

---
+++ post.php
@@ -0,0 +1,10 @@
+<?php
+
+    $name =         $_POST['name'];
+    $email =        $_POST['email'];
+    $address =      $_POST['address'];
+    $number =       $_POST['number'];
+    
+    echo "<br/> <br/> $name from $address";
+    
+?>
