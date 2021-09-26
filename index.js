93238c891891058fb90e9a37110877e8cbc4032e
Clay Flannery <johndoe@example.comcflannery94@gmail.com>
2021-09-26 19:39:34 -0400

    gsap

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".display-4", {
-    scrollTrigger: '.display-4',
-    x: 500
-} )
+gsap.from('.navbar-brand' , {
+    //scrollTrigger: '.logo',
+    x: 500,
+    duration:.3,
+    ease: 'power4'
+});
+
+gsap.from('.jumbotron' ,  {
+    opacity:0,
+    duration:.3,
+    delay: .25,
+    //stagger:.5
+});
+
+gsap.to('.gallery', {
+    scrollTrigger: {
+        trigger: '.gallery',
+        start: 'top 80%',
+        end: 'bottom 20%',
+        scrub:1,
+        markers:true,
+    },
+    rotation:360,
+    duration:.5,
+    stagger:.25
+    //ease: 'power4'
+});
+
+gsap.from('.row1' ,  {
+    scrollTrigger: {
+        trigger: '.row1',
+        start: 'top 80%',
+        end: 'bottom 20%',
+        markers:true,
+    },
+    opacity:0,
+    duration:.3,
+    //stagger:.5
+});
